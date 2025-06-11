d0e16519ee21af21667ef22ff36ecd7a9935dfe1
Rafael Carneiro <rafaelcarneiro1612@gmail.com>
2025-06-10 21:33:03 -0300

    Vidas

--- codigo/vidas.py
+++ codigo/vidas.py
@@ -1,10 +1,12 @@
 import pygame
 
-class Vidas():
-    def __init__(self, qtd_vidas:int):
+class Vidas(pygame.sprite.Sprite):
+    def __init__(self, qtd_vidas: int, grupo):
+        super().__init__(grupo)
         self.__qtd_vidas = qtd_vidas
-        self.__imagem = pygame.image.load(rf'C:\GitHub\Jogo\imagens\coracao.png')
-        self.__imagem_redimensionada = pygame.transform.scale(self.__imagem, (50, 50))
+        self.__imagem = pygame.image.load(r'C:\GitHub\Jogo\imagens\coracao.png').convert_alpha()
+        self.__image = pygame.transform.scale(self.__imagem, (50, 50))
+        self.__rect = self.__image.get_rect(topright = (800, 0))
 
     @property 
     def qtd_vidas(self): 
@@ -14,22 +16,27 @@ class Vidas():
     def qtd_vidas(self, valor):
         self.__qtd_vidas = valor
 
-    @property 
-    def imagem(self): 
-        return self.__imagem
+    @property
+    def image(self):
+        return self.__image
     
-    @imagem.setter
-    def imagem(self, valor):
-        self.__imagem = valor
+    @image.setter
+    def image(self, novo):
+        self.__image = novo
 
-    @property 
-    def imagem_redimensionada(self): 
-        return self.__imagem_redimensionada
+    @property
+    def rect(self):
+        return self.__rect
     
-    @imagem_redimensionada.setter
-    def imagem_redimensionada(self, valor):
-        self.__imagem_redimensionada = valor
+    @rect.setter
+    def rect(self, novo):
+        self.__rect = novo
+    
 
-    def exibir_vidas(self):
+    def exibir_vidas(self, tela, largura_tela):
         
-        
+        espaco = 10  
+        for i in range(self.__qtd_vidas):
+            x = largura_tela - ((i + 1) * (self.image.get_width() + espaco))
+            y = 10 
+            tela.blit(self.image, (x, y))
